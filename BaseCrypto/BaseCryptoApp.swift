a483ca44392765c961416142efbd05db8bf4c799
Nguyễn Trần Dũng <trandung1128@gmail.com>
2024-11-08 08:44:04 +0700

    Initial Commit

---
+++ BaseCrypto/BaseCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseCryptoApp.swift
+//  BaseCrypto
+//
+//  Created by Dũng Nguyễn on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BaseCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
